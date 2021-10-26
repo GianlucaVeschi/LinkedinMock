52901622042d1ca1e78394f6d6087aaddf2e7093
g.veschi <g.veschi@thalia.de>
2021-10-26 12:06:04 +0200

    Add Timber

--- app/src/main/java/com/gianlucaveschi/BaseApplication.kt
+++ app/src/main/java/com/gianlucaveschi/BaseApplication.kt
@@ -1,7 +1,18 @@
 package com.gianlucaveschi
 
 import android.app.Application
+import com.gianlucaveschi.linkedinmock.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
-class BaseApplication : Application()
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
